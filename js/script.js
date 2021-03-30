169b5dd39cc4d25d0277adf9be92d00df3853408
Aden Hanke <adenhankebiz@gmail.com>
2021-03-30 16:34:04 -0400

    show owned upgrades

--- js/script.js
+++ js/script.js
@@ -32,14 +32,17 @@ const upgrades = [
 ];
 // Generate buy menu here
 for (let i = 0; i < upgrades.length; i++) {
-    document.querySelector('div').innerHTML += `<button type="button" class="${upgrades[i].name}" onclick="buyMenu(${i})">${upgrades[i].name} $:${upgrades[i].price}</button>`
+    document.querySelector('.buymenu').innerHTML += `<button type="button" class="${upgrades[i].name} buybutton" onclick="buyMenu(${i})">${upgrades[i].name} $:${upgrades[i].price}</button>`
+}
+for (let i = 0; i < upgrades.length; i++) {
+    document.querySelector('.total').innerHTML += `<div><img src="${upgrades[i].image}" alt="${upgrades[i].name}" class="objects"><p class="img${upgrades[i].name} ownedTotal">x0</p></div>`
 }
 setInterval(micePerSecond, 1000)
 // this is probably bad implamentation. calling a for loop every second^
 function micePerSecond() {
     for (let i = 0; i < upgrades.length; i++) {
         currentMice += upgrades[i].owned * upgrades[i].mps;
-        document.querySelector('h3').innerHTML = `${currentMice} Mice`
+        document.querySelector('.count').innerHTML = `${currentMice} Mice`
     }
 }
 function buyMenu(index) {
@@ -47,10 +50,11 @@ function buyMenu(index) {
         currentMice -= Math.round(upgrades[index].price * ((costScale * upgrades[index].owned) + 1))
         upgrades[index].owned++
         document.querySelector(`.${upgrades[index].name}`).innerHTML = `${upgrades[index].name} $:${Math.round(upgrades[index].price * ((costScale * upgrades[index].owned) + 1))}`
-        document.querySelector('h3').innerHTML = `${currentMice} Mice`
+        document.querySelector(`.img${upgrades[index].name}`).innerHTML = `x${upgrades[index].owned}`
+        document.querySelector('.count').innerHTML = `${currentMice} Mice`
     }
 }
 function mouseClick() {
     currentMice++
-    document.querySelector('h3').innerHTML = `${currentMice} Mice`
+    document.querySelector('.count').innerHTML = `${currentMice} Mice`
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-The goal of this project is to make a clicker game about computer mice and the implict humor that comes from that situation.
+The goal of this project is to make a clicker game about computer mice that meets the project requirements for code louisville front end web development.
+# CSS Features
+- Use Flexbox or CSS Grid to organize content areas based on mobile or desktop views.
+- Use “:nth-child” or “nth-of-type” to style a series of elements on your page
+# JavaScript Features
+- Create a JavaScript function that performs some form of mathematical operation (calculates something) and displays the result on your page or otherwise uses that value to do something on the site.
+- Create one or more JavaScript objects (such as with multiple values contained in { }’s), modify at least one value of that object, and display it on your site
